1f7bc78785b90387640e03a75c9a72534cc92ff2
Andrew Sima <andrew.sima@robinhood.com>
2022-03-07 12:43:10 -0500

    compiling now

--- distributed_sim/mapreduce.py
+++ distributed_sim/mapreduce.py
@@ -7,12 +7,14 @@ class MRProcedure:
 
     def __init__(self,
                  num_machines: int,
+                 max_distance: int,
                  n_records: int,
-                 prog_type: MReduceProg = MReduceProg.sort):
+                 prog_type: MReduceProg):
         self.num_machines = num_machines
+        self.max_distance = max_distance
         self.n_records = n_records
         self.prog = prog_type
-        self.workers = seed_workers(num_machines)
+        self.workers = seed_workers(num_machines, max_distance)
         self.task_graph = None
         if self.prog == MReduceProg.distributedgrep:
             self.task_graph = self._build_grep(
@@ -82,8 +84,8 @@ class MRProcedure:
                 Task(parent_prog=MReduceProg.distributedsort,
                      task_id=i+num_mach+1, task_op=MReduceOp.reduce, n_records=dist))
         t.add_layer(reducetask_list, starting_idx=num_mach+1,
-                    option=TaskLayerChoices.one_to_one)
+                    option=TaskLayerChoices.fully_connected)
         return t
 
-    def execute(self):
+    def run(self):
         self.scheduler.simulate()
