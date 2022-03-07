7da89d1f86ad3aada889b9ab96f41a0069a35dcd
Albert Zhang <albertzhang9000@gmail.com>
2022-03-07 16:41:47 -0500

    fix merge

--- scripts/terminal.py
+++ scripts/terminal.py
@@ -9,19 +9,11 @@ def system_input():
         click.echo(f"[{j.value}] {j.name}")
     sys_value = click.prompt('Please select a system', type=int)
     click.echo("Available Procedures: ")
-<<<<<<< HEAD
-    if sys_value == SystemOptions.mapreduce.value:
-        for j in MReduceProg:
-            click.echo(f"[{j.value}] {j.name}")
-    elif sys_value == SystemOptions.dask.value:
-        for j in DaskProg:
-=======
     if sys_value == SystemOptions.dask.value:
         for j in DaskProg:
             click.echo(f"[{j.value}] {j.name}")
     elif sys_value == SystemOptions.mapreduce.value:
         for j in MReduceProg:
->>>>>>> 2ae47f32b3a87439d33a018d464447bc1383ff47
             click.echo(f"[{j.value}] {j.name}")
     prog_value = click.prompt('Please select a procedure', type=int)
     if sys_value == SystemOptions.mapreduce.value:
