91bb811cad2a4eeb3a361f78bdc088bd64b44158
Sanket Jayant Purandare <sanketpurandare@g.harvard.edu>
2022-03-09 11:21:34 -0500

    "Updating test"

--- scripts/test.py
+++ scripts/test.py
@@ -4,13 +4,8 @@ from distributed_sim import *
 
 
 def main():
-<<<<<<< HEAD
-    mr = MRProcedure(num_machines=100,
-                     n_records=10000,
-=======
     mr = MRProcedure(num_machines=1000,
                      n_records=1000000,
->>>>>>> 0bf8b3bb1ea76de377e44012568e109bc7a358f4
                      max_distance=10,
                      prog_type=MReduceProg.distributedsort)
     mr.run()
